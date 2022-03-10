60d86062c242832adba7b30150d2cf525373301a
Timbuktujoe <23214407+Timbuktujoe@users.noreply.github.com>
2022-03-10 20:05:36 +0100

    add mockdata messages

--- data/dummyMessages.js
+++ data/dummyMessages.js
@@ -1,60 +1,55 @@
-const messages = [
-    {
-        id: 1,
-        text: "Strain be thirsty Goblinses. Barahir lied answer darkest dealing he's collared uttered invented likely while.",
-        date: "10 October 2022",
-        id_user: 1,
-        image_url: 'https://placedog.net/200'
-    },
-    {
-        id: 2,
-        text: "Insist blazing fine weapon son's pouch perfectly Meduseld escapes. Bereft waking wind vouch didn't mash smell rings Théodred's trade.",
-        date: "12 October 2022",
-        id_user: 2,
-        image_url: 'https://placedog.net/200'
-    },
-    {
-        id: 3,
-        text: "Out-of-pocket borders swore lots pig sung Gondor you'll! Favored flame victory Ered aiding caution tastes couldn't there's member somewhat champ!",
-        date: "14 October 2022",
-        id_user: 3,
-        image_url: 'https://placedog.net/200'
-    },
-    {
-        id: 4,
-        text: "Trick served together birds ought Dory vest pages. There is only one Lord of the Ring.",
-        date: "10 October 2022",
-        id_user: 4,
-        image_url: 'https://placedog.net/200'
-    },
-    {
-        id: 5,
-        text: "Dunland joking up. Except rock Anduin bottles herald 111. ",
-        date: "10 October 2022",
-        id_user: 5,
-        image_url: 'https://placedog.net/200'
-    },
-    {
-        id: 6,
-        text: "Morning shh Boromir's 22nd backs gongs rule summon emptied regain.",
-        date: "2021-10-11",
-        id_user: 6,
-        image_url: 'https://placedog.net/200'
-    },
-    {
-        id: 7,
-        text: "It must be taken deep into Mordor and cast back into the fiery chasm from whence it came.",
-        date: "2021-10-11",
-        id_user: 1,
-        image_url: 'https://placedog.net/200'
-    },
-    {
-        id: 8,
-        text: "Stead n Hobbits. Doubted Rauros potent?",
-        date: "2021-10-11",
-        id_user: 2,
-        image_url: 'https://placedog.net/200'
-    }
-]
- 
-module.exports = { messages }
+const messages = [{
+    text: "potenti nullam porttitor lacus at turpis donec posuere metus vitae ipsum",
+    date: "6/11/2018",
+    user_id: 1,
+    picture: "http://dummyimage.com/170x146.png/ff4444/ffffff"
+}, {
+    text: "molestie hendrerit at vulputate vitae nisl aenean lectus pellentesque eget nunc donec quis orci",
+    date: "8/1/2016",
+    user_id: 2,
+    picture: "http://dummyimage.com/122x151.png/5fa2dd/ffffff"
+}, {
+    text: "vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae nulla dapibus dolor vel est donec odio",
+    date: "12/3/2015",
+    user_id: 3,
+    picture: "http://dummyimage.com/236x165.png/ff4444/ffffff"
+}, {
+    text: "sollicitudin ut suscipit a feugiat et eros vestibulum ac est lacinia nisi venenatis",
+    date: "3/28/2018",
+    user_id: 4,
+    picture: "http://dummyimage.com/117x213.png/dddddd/000000"
+}, {
+    text: "massa id nisl venenatis lacinia aenean sit amet justo morbi ut odio cras mi pede",
+    date: "2/14/2017",
+    user_id: 5,
+    picture: "http://dummyimage.com/138x171.png/5fa2dd/ffffff"
+}, {
+    text: "lobortis convallis tortor risus dapibus augue vel accumsan tellus nisi eu",
+    date: "5/9/2018",
+    user_id: 6,
+    picture: "http://dummyimage.com/229x211.png/ff4444/ffffff"
+}, {
+    text: "amet diam in magna bibendum imperdiet nullam orci pede venenatis non",
+    date: "8/26/2019",
+    user_id: 7,
+    picture: "http://dummyimage.com/128x101.png/cc0000/ffffff"
+}, {
+    text: "consequat nulla nisl nunc nisl duis bibendum felis sed interdum venenatis turpis enim blandit mi in porttitor pede",
+    date: "12/5/2020",
+    user_id: 8,
+    picture: "http://dummyimage.com/232x228.png/cc0000/ffffff"
+}, {
+    text: "lacus curabitur at ipsum ac tellus semper interdum mauris ullamcorper purus sit amet nulla",
+    date: "12/4/2016",
+    user_id: 9,
+    picture: "http://dummyimage.com/115x138.png/dddddd/000000"
+}, {
+    text: "porta volutpat quam pede lobortis ligula sit amet eleifend pede libero quis orci nullam molestie nibh in lectus pellentesque at",
+    date: "6/29/2021",
+    user_id: 10,
+    picture: "http://dummyimage.com/123x217.png/cc0000/ffffff"
+}]
+
+module.exports = {
+    messages
+}
