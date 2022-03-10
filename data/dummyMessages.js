6ce9249feb88dcebff9f065c326182f8fabc705a
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-03-10 19:55:54 +0100

    initial commit

---
+++ data/dummyMessages.js
@@ -0,0 +1,60 @@
+const messages = [
+    {
+        id: 1,
+        text: "Strain be thirsty Goblinses. Barahir lied answer darkest dealing he's collared uttered invented likely while.",
+        date: "10 October 2022",
+        id_user: 1,
+        image_url: 'https://placedog.net/200'
+    },
+    {
+        id: 2,
+        text: "Insist blazing fine weapon son's pouch perfectly Meduseld escapes. Bereft waking wind vouch didn't mash smell rings Théodred's trade.",
+        date: "12 October 2022",
+        id_user: 2,
+        image_url: 'https://placedog.net/200'
+    },
+    {
+        id: 3,
+        text: "Out-of-pocket borders swore lots pig sung Gondor you'll! Favored flame victory Ered aiding caution tastes couldn't there's member somewhat champ!",
+        date: "14 October 2022",
+        id_user: 3,
+        image_url: 'https://placedog.net/200'
+    },
+    {
+        id: 4,
+        text: "Trick served together birds ought Dory vest pages. There is only one Lord of the Ring.",
+        date: "10 October 2022",
+        id_user: 4,
+        image_url: 'https://placedog.net/200'
+    },
+    {
+        id: 5,
+        text: "Dunland joking up. Except rock Anduin bottles herald 111. ",
+        date: "10 October 2022",
+        id_user: 5,
+        image_url: 'https://placedog.net/200'
+    },
+    {
+        id: 6,
+        text: "Morning shh Boromir's 22nd backs gongs rule summon emptied regain.",
+        date: "2021-10-11",
+        id_user: 6,
+        image_url: 'https://placedog.net/200'
+    },
+    {
+        id: 7,
+        text: "It must be taken deep into Mordor and cast back into the fiery chasm from whence it came.",
+        date: "2021-10-11",
+        id_user: 1,
+        image_url: 'https://placedog.net/200'
+    },
+    {
+        id: 8,
+        text: "Stead n Hobbits. Doubted Rauros potent?",
+        date: "2021-10-11",
+        id_user: 2,
+        image_url: 'https://placedog.net/200'
+    }
+]
+ 
+module.exports = { messages }
---
+++ data/dummyUsers.js
@@ -0,0 +1,54 @@
+const users = 
+
+[
+	{
+		id: 1,
+		name: 'Reine Tresise',
+		handle: 'rtresise0',
+		city: 'Concepción del Bermejo',
+		joined: '5/6/2021',
+		avatar: 'http://dummyimage.com/145x100.png/dddddd/000000',
+	},
+	{
+		id: 2,
+		name: 'Dale Pioch',
+		handle: 'dpioch1',
+		city: 'Lérida',
+		joined: '7/31/2021',
+		avatar: 'http://dummyimage.com/212x100.png/dddddd/000000',
+	},
+	{
+		id: 3,
+		name: 'Jesselyn Starie',
+		handle: 'jstarie2',
+		city: 'Vihāri',
+		joined: '3/25/2021',
+		avatar: 'http://dummyimage.com/223x100.png/ff4444/ffffff',
+	},
+	{
+		id: 4,
+		name: 'Lewiss Yerrall',
+		handle: 'lyerrall3',
+		city: 'Salaspils',
+		joined: '9/6/2021',
+		avatar: 'http://dummyimage.com/238x100.png/dddddd/000000',
+	},
+	{
+		id: 5,
+		name: 'Leicester Franciskiewicz',
+		handle: 'lfranciskiewicz4',
+		city: 'Al Marsá',
+		joined: '8/19/2021',
+		avatar: 'http://dummyimage.com/133x100.png/ff4444/ffffff',
+	},
+	{
+		id: 6,
+		name: 'Ashla Rousel',
+		handle: 'arousel5',
+		city: 'Ekerö',
+		joined: '8/27/2021',
+		avatar: 'http://dummyimage.com/149x100.png/cc0000/ffffff',
+	},
+]
+
+module.exports = { users }
