2dce091ff214b7f0947b7d205ec0bb783c9f2c24
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-03-10 20:45:35 +0100

    create user routes

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ const port = process.env.port || 8585
 require('dotenv').config()
 const cors = require('cors')
 const connectToDB = require('./models')
-app.use(express.json())
-app.use(cors())
+const userRouter = require('./routes/User')
 
 app.use((request, result, next) => {
     console.log(request.method, request.path, request.sessionID)
     next()
 })
 
+app.use(express.json())
+app.use(cors())
+
+app.use((error, request, result, next) => {
+    console.error(error.message)
+    result.status(500).send('Something broke!')
+})
+
+app.use('/users', userRouter)
 
 connectToDB().then(() => {
     app.listen(port, () => console.log(`Started listening on PORT ${port}`))
