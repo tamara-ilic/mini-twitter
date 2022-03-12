eb84fdece87099e72cb9f40d06098b9fd2bcecfb
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-03-12 11:42:40 +0100

    update port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 8585
+const port = process.env.PORT || 8585
 require('dotenv').config()
 const connectToDB = require('./models')
 const cors = require('cors')
