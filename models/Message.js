6bd7bf9fdd1c997d6bac965d0cf1a987fa19d8bc
Timbuktujoe <23214407+Timbuktujoe@users.noreply.github.com>
2022-03-12 14:21:06 +0100

    populate message with user_id

--- models/Message.js
+++ models/Message.js
@@ -4,11 +4,15 @@ const MessageSchema = new mongoose.Schema(
     {
         user_handle: { required: true, type: String },
         text: { required: true, type: String },
-        user_id: { required: true, type: mongoose.Schema.Types.ObjectId },
+        user_id: { required: true, type: mongoose.Schema.Types.ObjectId, ref: 'User' },
         picture: { required: false, type: String }
     },
     { timestamps: true }
 );
+MessageSchema.pre('find', function () {
+    this.populate('user_id')
+})
+
 
 const Message = mongoose.model('Message', MessageSchema)
 
