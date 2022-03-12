9df44e0389caae943b5522708fda8fcdb4879930
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-03-12 10:35:48 +0100

    remove test logs

--- controllers/User.js
+++ controllers/User.js
@@ -8,9 +8,7 @@ async function getAllUsers(request, response, next) {
 }
 
 async function getUser(request, response) {
-    console.log('we are here')
     const user = await User.find({ _id: request.params.id })
-    // const user = await User.find({  "name": "Netty Portress" })
     console.log(request.params.id);
     response.json(user)
 }
